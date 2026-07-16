9294c8f06b06df2b75657687f0468d0220cbbcc8
prem-programs <premolearn@gmail.com>
2026-07-16 12:13:04 +0530

    hero page fullscreen

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -9,7 +9,7 @@ const Statistics = () => {
   ];
 
   return (
-    <section className="py-20 bg-off-white relative z-20 -mt-8">
+    <section className="py-20 bg-off-white relative z-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {stats.map((stat, index) => (
